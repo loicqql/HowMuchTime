d970a0b6037b3d05aa3d85bc6181ebe559e00bb2
loic quinquenel <loic.qql@gmail.com>
2021-05-02 11:38:29 +0200

    feat: Added style and dark color

--- src/js/stats.js
+++ src/js/stats.js
@@ -3,6 +3,7 @@
 displayData();
 updateBlockedWebsites();
 updateCheckboxDark();
+displayTotalTime();
 
 document.getElementById("export").addEventListener("click", exportData);
 document.getElementById("importInput").addEventListener("change", importData);
@@ -23,15 +24,16 @@ ths.forEach(tr => {
     tr.addEventListener('click', selectTable);
 });
 
-document.querySelector('header img').addEventListener('click', () => {
+document.querySelector('header .title h1').addEventListener('click', () => {
     document.querySelector('header li:first-of-type').click();
 });
 
-document.addEventListener('visibilitychange', function(e) {
+document.addEventListener('visibilitychange', function (e) {
     if (document.visibilityState == "visible") {
         displayData();
         updateBlockedWebsites();
         updateCheckboxDark();
+        displayTotalTime();
     }
 });
 
@@ -39,20 +41,20 @@ function displayData() {
     let tbody = document.querySelector('.index tbody');
     tbody.innerHTML = "";
 
-    getAllData((data) => {
+    getAllData((data) => {
 
         data.shift();
 
         data.sort((a, b) => {
             return getTimeByWebsite(popKey(b)) - getTimeByWebsite(popKey(a));
         });
-        
+
         data.forEach(row => {
 
             let key = Object.keys(row);
 
             row = row[key[0]];
-            
+
             let tr = document.createElement('tr');
             tr.dataset.domain = key;
 
@@ -82,10 +84,10 @@ function getTimeByWebsite(rows) {
     let times = 0;
 
     rows.forEach(row => {
-        if((row['start'] != null) && (row['end'] != null)) {
+        if ((row['start'] != null) && (row['end'] != null)) {
             let time = row['end'] - row['start'];
-            if(time > TOLERANCE) {
-                times  = times + time;
+            if (time > TOLERANCE) {
+                times = times + time;
             }
         }
     });
@@ -93,24 +95,51 @@ function getTimeByWebsite(rows) {
     return times;
 }
 
+function getTotalTime(callback) {
+
+    getAllData((rows) => {
+        let times = 0;
+
+        for (let i = 1; i < rows.length; i++) {
+            const row = rows[i];
+
+            let key = Object.keys(row);
+
+            let sessions = row[key[0]];
+
+            sessions.forEach(session => {
+                if ((session['start'] != null) && (session['end'] != null)) {
+                    let time = session['end'] - session['start'];
+                    if (time > TOLERANCE) {
+                        times = times + time;
+                    }
+                }
+            });
+        }
+
+        callback(formatTime(times));
+    })
+}
+
 function exportData() {
+
     browser.storage.local.get([KEY]).then((e) => {
-        if(e.hasOwnProperty(KEY)) {
+        if (e.hasOwnProperty(KEY)) {
             var element = document.createElement('a');
             element.setAttribute('href', 'data:application/JSON;charset=utf-8,' + encodeURIComponent(JSON.stringify(e)));
-            element.setAttribute('download', KEY+'.json');
-              
+            element.setAttribute('download', KEY + '.json');
+
             element.style.display = 'none';
             document.body.appendChild(element);
-              
+
             element.click();
-              
-            document.body.removeChild(element);              
+
+            document.body.removeChild(element);
         }
     },
-    function(e) {
-        console.log(`Error: ${e}`);
-    });
+        function (e) {
+            console.log(`Error: ${e}`);
+        });
 }
 
 function importData() {
@@ -121,12 +150,12 @@ function importData() {
     fr.addEventListener("load", e => {
         try {
             let json = JSON.parse(fr.result);
-            if(checkJsonData(json)) {
+            if (checkJsonData(json)) {
                 updateData(json);
-            }else {
+            } else {
                 displayMessage('An error has occurred');
             }
-        }catch(e) {
+        } catch (e) {
             displayMessage('An error has occurred');
         }
     });
@@ -138,40 +167,40 @@ function checkJsonData(json) {
 
     try {
 
-        if(!json.hasOwnProperty(KEY)) {
+        if (!json.hasOwnProperty(KEY)) {
             return false;
         }
-    
+
         data = json[KEY];
 
         let valid = true;
 
         for (let i = 1; i < data.length; i++) {
             const el = data[i];
-            
+
             let key = Object.keys(el);
 
             key = key[0];
-            
+
             let row = el[key];
 
             row.forEach(time => {
 
-                if(time.hasOwnProperty("start") && time.hasOwnProperty("end")) {
+                if (time.hasOwnProperty("start") && time.hasOwnProperty("end")) {
                     let start = time['start'];
                     let end = time['end'];
 
-                    if(Number.isInteger(start) && Number.isInteger(end)) {
-                        if(start >= end) {
+                    if (Number.isInteger(start) && Number.isInteger(end)) {
+                        if (start >= end) {
                             valid = false;
                         }
-                    }else {
-                        if(!Number.isInteger(start)) {
+                    } else {
+                        if (!Number.isInteger(start)) {
                             valid = false;
                         }
                     }
 
-                }else {
+                } else {
                     valid = false;
                 }
             });
@@ -179,7 +208,7 @@ function checkJsonData(json) {
 
         return valid;
 
-    } catch(error) {
+    } catch (error) {
         console.log(error);
 
         return false;
@@ -188,27 +217,41 @@ function checkJsonData(json) {
 
 function updateData(json) {
     console.log(json);
-    displayMessage('Data has been updated');
+    browser.storage.local.set({
+        [KEY]: json[KEY]
+    }).then(() => {
+        displayData();
+        updateBlockedWebsites();
+        updateCheckboxDark();
+        displayTotalTime();
+        displayMessage('Data has been updated');
+    })
+    
 }
 
 function displayMessage(e) {
-    console.log(e)
+    document.querySelector('.message p').textContent = e;
+    let message = document.querySelector('.message');
+    message.classList.add('active');
+    setTimeout(() => {
+        message.classList.remove('active');
+    }, 5000)
 }
 
 
 function selectTable() {
-    if(this.classList.contains('select')) {
+    if (this.classList.contains('select')) {
 
         let i = this.querySelector('i');
-        if(i.classList.contains('fa-caret-up')) {
+        if (i.classList.contains('fa-caret-up')) {
             i.classList.remove('fa-caret-up');
             i.classList.add('fa-caret-down');
-        }else {
+        } else {
             i.classList.remove('fa-caret-down');
             i.classList.add('fa-caret-up');
         }
 
-    }else {
+    } else {
         let ths = document.querySelectorAll('.index th');
         ths.forEach(tr => {
             tr.classList.remove('select');
@@ -218,16 +261,16 @@ function selectTable() {
 
         let i = this.querySelector('i');
 
-        if(i.classList.contains('fa-caret-down')) {
+        if (i.classList.contains('fa-caret-down')) {
             i.classList.remove('fa-caret-down');
             i.classList.add('fa-caret-up');
         }
     }
 
     let i = this.querySelector('i');
-    if(i.classList.contains('fa-caret-down')) {
+    if (i.classList.contains('fa-caret-down')) {
         sortTable(this.dataset.column, true);
-    }else {
+    } else {
         sortTable(this.dataset.column, false);
     }
 
@@ -243,10 +286,10 @@ function sortTable(column, ASC) {
     }
 
     const extractData = (e) => {
-        if(column === "1") { //sort name of websites
-            return urlToNumber(e.querySelector('td:nth-child('+column+')').dataset.raw);
-        }else {
-            return e.querySelector('td:nth-child('+column+')').dataset.raw;
+        if (column === "1") { //sort name of websites
+            return urlToNumber(e.querySelector('td:nth-child(' + column + ')').dataset.raw);
+        } else {
+            return e.querySelector('td:nth-child(' + column + ')').dataset.raw;
         }
     }
 
@@ -254,11 +297,11 @@ function sortTable(column, ASC) {
 
     trs = Array.prototype.slice.call(trs, 0);
 
-    if(ASC) {
+    if (ASC) {
         trs.sort((a, b) => {
             return extractData(a) - extractData(b);
         });
-    }else {
+    } else {
         trs.sort((a, b) => {
             return extractData(b) - extractData(a);
         });
@@ -270,7 +313,7 @@ function sortTable(column, ASC) {
     trs.forEach(tr => {
         tbody.appendChild(tr);
     });
-    
+
 }
 
 function navigation() {
@@ -285,18 +328,18 @@ function navigation() {
     for (let i = 0; i < sections.length; i++) {
         sections[i].style.display = 'none';
     }
-    
-    let section = document.querySelector('.'+this.dataset.section);
+
+    let section = document.querySelector('.' + this.dataset.section);
     section.style.display = 'block';
 
     let func = new functions()
     func[this.dataset.section]();
 }
 
-function functions(){
-    this.index = function(){displayData()};
-    this.settings = function(){updateBlockedWebsites()};
-    this.about = function(){};
+function functions() {
+    this.index = function () { displayData() };
+    this.settings = function () { updateBlockedWebsites() };
+    this.about = function () { };
 }
 
 function searchBar() {
@@ -306,65 +349,69 @@ function searchBar() {
     trs.forEach(tr => {
         tr.style.display = 'table-row';
 
-        if(input != "") {
-                if(!(tr.dataset.domain.toLowerCase().includes(input.toLowerCase()))) {
+        if (input != "") {
+            if (!(tr.dataset.domain.toLowerCase().includes(input.toLowerCase()))) {
                 tr.style.display = "none";
             }
         }
-        
-    });    
+
+    });
 }
 
 function setBlockedWebsite(domain, subdomains, callback) {
-    getAllData((data)=>{
+    getAllData((data) => {
 
         let updated = false;
         data[0]['blockedWebsites'].forEach(el => {
-            if(el.domain === domain) {
+            if (el.domain === domain) {
                 updated = true;
                 el.subdomains = subdomains;
             }
         });
 
-        if(!updated) {
+        if (!updated) {
             data[0]['blockedWebsites'].push({
                 domain: domain,
                 subdomains: subdomains
             });
         }
-        
+
         browser.storage.local.set({
-            [KEY] : data
+            [KEY]: data
         }).then(() => {
             updateBlockedWebsites();
+            displayMessage('Data has been updated');
             callback();
         });
     });
 }
 
 function updateBlockedWebsites() {
-    getAllData((e)=>{
+    getAllData((e) => {
         let tbody = document.querySelector('.settings tbody');
         tbody.innerHTML = '';
 
-        if(e[0]['blockedWebsites'].length == 0) {
+        if (e[0]['blockedWebsites'].length == 0) {
             let td = document.createElement("td");
             td.textContent = 'No blocked websites';
             tbody.appendChild(td);
-        }else {
+        } else {
             e[0]['blockedWebsites'].forEach(el => {
                 let tr = document.createElement("tr");
                 let td = document.createElement("td");
                 let p = document.createElement("p");
-                
-                if(el.subdomains) {
-                    p.textContent = el.domain + ' with subdomains';
-                }else {
+
+                let span;
+                if (el.subdomains) {
+                    p.textContent = el.domain;
+                    span = document.createElement("span");
+                    span.textContent = '+ subdomains'
+                } else {
                     p.textContent = el.domain;
                 }
-                
+
                 let i = document.createElement("i");
-                i.classList.add('fas', 'fa-trash-alt');
+                i.classList.add('fas', 'fa-times');
                 i.dataset.raw = el.domain;
 
                 i.addEventListener('click', (e) => {
@@ -372,6 +419,9 @@ function updateBlockedWebsites() {
                 });
 
                 p.appendChild(i);
+                if (el.subdomains) {
+                    p.appendChild(span);
+                }
                 td.appendChild(p);
                 tr.appendChild(td);
                 tbody.appendChild(tr);
@@ -381,11 +431,11 @@ function updateBlockedWebsites() {
 }
 
 function deleteBlockedWebsite(domain) {
-    getAllData((data)=>{
+    getAllData((data) => {
         data[0]['blockedWebsites'] = data[0]['blockedWebsites'].filter(item => item.domain !== domain);
 
         browser.storage.local.set({
-            [KEY] : data
+            [KEY]: data
         }).then(() => {
             updateBlockedWebsites();
         });
@@ -401,10 +451,10 @@ function formWebsiteBlocked(e) {
     //possibilité de bloquer des sous domaines sur une un domaine qui contient déja un sous domaine ?
 
     setBlockedWebsite(domain, document.getElementById('subdomains').checked, () => {
-        if(document.getElementById('deleteData').checked) {
+        if (document.getElementById('deleteData').checked) {
             deleteDataByWebsite(domain);
         }
-    });   
+    });
 }
 
 function deleteDataByWebsite(website) {
@@ -414,15 +464,15 @@ function deleteDataByWebsite(website) {
 
         const getKey = (row) => {
             let key = Object.keys(row);
-            if(key.length > 1) {
-                key[0] = "How: "+Math.random()*10 //Never mind
+            if (key.length > 1) {
+                key[0] = "How: " + Math.random() * 10 //Never mind
             }
             return key[0];
         }
 
-        if(deleteSubdomains) {
+        if (deleteSubdomains) {
             data = data.filter(item => !getKey(item).includes(website));
-        }else {
+        } else {
             data = data.filter(item => getKey(item) != website);
         }
 
@@ -435,16 +485,24 @@ function deleteDataByWebsite(website) {
 }
 
 function updateDark() {
-    getAllData((data)=>{
+    getAllData((data) => {
         data[0]['is_dark'] = document.querySelector(".checkbox_dark").checked;
+        data[0]['is_dark'] ? document.querySelector(".stats").classList.add('dark') : document.querySelector(".stats").classList.remove('dark');
         browser.storage.local.set({
-            [KEY] : data
+            [KEY]: data
         })
     });
 }
 
 function updateCheckboxDark() {
-    getAllData((data)=>{
+    getAllData((data) => {
         document.querySelector(".checkbox_dark").checked = data[0]['is_dark'];
+        data[0]['is_dark'] ? document.querySelector(".stats").classList.add('dark') : document.querySelector(".stats").classList.remove('dark');
+    });
+}
+
+function displayTotalTime() {
+    getTotalTime((result) => {
+        document.querySelector("header .title p").textContent = result;
     });
 }
